a2031395f0103e40d571a5049167aa584043fc9a
Oğuzhan Karakuş <oguzhan.karakus@mobillium.com>
2020-12-15 14:01:39 +0300

    Initial Commit

---
+++ fastlane-tutorialTests/fastlane_tutorialTests.swift
@@ -0,0 +1,33 @@
+//
+//  fastlane_tutorialTests.swift
+//  fastlane-tutorialTests
+//
+//  Created by Oğuzhan Karakuş on 15.12.2020.
+//
+
+import XCTest
+@testable import fastlane_tutorial
+
+class fastlane_tutorialTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
